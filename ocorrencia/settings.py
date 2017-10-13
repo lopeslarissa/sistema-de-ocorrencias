3c6e1012a44fc9eec679a5b0afeab021c3ef5598
Larissa <lopes.larissalopes@gmail.com>
2017-10-12 23:45:40 -0300

    configuração dos arquivos de tradução

--- ocorrencia/settings.py
+++ ocorrencia/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -130,3 +132,13 @@ STATICFILES_DIRS = (
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
+
+from django.utils.translation import ugettext_lazy as _
+LANGUAGES = (
+    ('en', _('English')),
+    ('pt-br', _('Portuguese')),
+)
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, '/sistema/locale'),
+)
--- ocorrencia/urls.py
+++ ocorrencia/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-# from sistema.urls import *
+from sistema.urls import *
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     url(r'^admin/', admin.site.urls),
     url(r'^', include('sistema.urls')),
-]
+)
