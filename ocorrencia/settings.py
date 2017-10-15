ed1a1f2498b2508b182382d9578d2bb23998d55a
Larissa <lopes.larissalopes@gmail.com>
2017-10-14 22:38:20 -0300

    debug = true

--- ocorrencia/settings.py
+++ ocorrencia/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '813&rla315a3kfb#xr@4vq#%+qk^#p*p7+m7geq9)+1(8jli(8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['http://lopes.pythonanywhere.com/']
 
